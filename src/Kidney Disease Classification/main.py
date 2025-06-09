525847b268d31ff9c7b2aa36cc55971c7fdd64de
chirag_2705 <csharma@kgpian.iitkgp.ac.in>
2025-06-09 10:40:09 +0530

    utils added

---
+++ src/Kidney Disease Classification/main.py
@@ -0,0 +1,3 @@
+from Kidney_Disease_Classification import logger
+
+logger.info("This is an info message from the Kidney Disease Classification module.")
---
+++ src/Kidney_Disease_Classification/main.py
@@ -0,0 +1,3 @@
+from src.Kidney_Disease_Classification import logger
+
+logger.info("This is an info message from the Kidney Disease Classification module.")
